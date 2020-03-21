46ed99fca12ed58932970f38d29da5134dc8f7b0
Felix Ebert <12857672+felix-ebert@users.noreply.github.com>
2020-03-21 16:12:11 +0100

    Add basic gastro material card

---
+++ src/components/GastroOverview.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <md-card md-with-hover>
+      <md-card-header>
+        <md-card-header-text>
+          <div class="md-title">{{gastro.name}}</div>
+        </md-card-header-text>
+
+        <md-card-media md-medium>
+          <img :src="require('../assets/img/gastro_logos/' + gastro.logo)" alt="People" />
+        </md-card-media>
+      </md-card-header>
+
+      <md-card-actions>
+        <md-button>Zum Angebot</md-button>
+      </md-card-actions>
+    </md-card>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.md-card {
+  width: 320px;
+  margin: 4px;
+  display: inline-block;
+  vertical-align: top;
+}
+</style>
+
+<script>
+export default {
+  name: "GastroOverview",
+  data: function() {
+    return {
+      gastro: {
+        name: "Reynabi",
+        contact: {
+          facebook: "https://www.facebook.com/KussmundGiessen/",
+          tel: "0641 2797720"
+        },
+        address: {
+          postal_code: 35394,
+          street: "Grünberger Str.",
+          number: "115"
+        },
+        logo: "reynabi.png"
+      }
+    };
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { MdCard, MdButton } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
 Vue.config.productionTip = false
+
+Vue.use(MdCard)
+Vue.use(MdButton)
 
 new Vue({
   render: h => h(App),
