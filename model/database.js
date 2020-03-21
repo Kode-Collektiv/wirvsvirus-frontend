7c308168b27a1b4700832569a7150ad99e91929d
niecore <nicomueller1991@googlemail.com>
2020-03-21 12:36:38 +0100

    Add first set of gastro data
    (cherry picked from commit 44a0110a5b03998b8ac01eaeda7b0f30ecdcbd07)

---
+++ model/database.js
@@ -0,0 +1,100 @@
+const gastronomy = [
+    {
+        name: "BAZAAR",
+        contact: {
+            tel: "017631355841",
+            instagram: "bazaar_giessen"
+        },
+        address: {
+            postal_code: 35398,
+            street: "Schützenstraße",
+            number: "1"
+        },
+        logo: "bazaar-logo-1.jpg"
+    },
+    {
+        name: "Bärenstark",
+        contact: {
+            tel: "0641 97274191",
+        },
+        address: {
+            postal_code: 35390,
+            street: "Katharinengasse",
+            number: "14"
+        },
+        logo: "baerenstark.png"
+    },
+    {
+        name: "Geschmacksverkehr",
+        contact: {
+            tel: "0641 20916995",
+        },
+        address: {
+            postal_code: 35390,
+            street: "Mäusburg",
+            number: "8"
+        },
+        logo: "geschmacksverkehr-e1584715926544-1024x1024.jpg"
+    },
+    {
+        name: "heisszeit",
+        contact: {
+            tel: "0641 58778002",
+        },
+        address: {
+            postal_code: 35390,
+            street: "Plockstraße",
+            number: "7"
+        },
+        logo: "heisszeit.png"
+    },
+    {
+        name: "Gutburgerlich",
+        contact: {
+            tel: "0641 2097059",
+        },
+        address: {
+            postal_code: 35390,
+            street: "Bahnhofstraße",
+            number: "22"
+        },
+        logo: "gutburgerlich_logo_schwarz-1024x1024.png"
+    },
+    {
+        name: "Noams",
+        contact: {
+            tel: "0641 58098301",
+        },
+        address: {
+            postal_code: 35390,
+            street: "Reichensand",
+            number: "11"
+        },
+        logo: "noams.png"
+    },
+    {
+        name: "Kussmund",
+        contact: {
+            facebook: "https://www.facebook.com/KussmundGiessen/",
+        },
+        address: {
+            postal_code: 35390,
+            street: "Katharinengasse",
+            number: "12"
+        },
+        logo: "kussmund-1024x1024.png"
+    },
+    {
+        name: "Reynabi",
+        contact: {
+            facebook: "https://www.facebook.com/KussmundGiessen/",
+            tel: "0641 2797720"
+        },
+        address: {
+            postal_code: 35394,
+            street: "Grünberger Str.",
+            number: "115"
+        },
+        logo: "reynabi.png"
+    },
+];
