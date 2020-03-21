e1df82d13d79fea01fab265ddccdddd4f0987b98
Felix Ebert <12857672+felix-ebert@users.noreply.github.com>
2020-03-21 17:21:23 +0100

    Add for loop to show all gastros

--- src/components/GastroOverview.vue
+++ src/components/GastroOverview.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <md-card md-with-hover>
+  <div class="gastro-overview">
+    <div v-for="gastro in gastronomy" :key="gastro.id">
+    <md-card  md-with-hover>
       <md-card-header>
         <md-card-header-text>
           <div class="md-title">{{gastro.name}}</div>
@@ -15,13 +16,28 @@
         <md-button>Zum Angebot</md-button>
       </md-card-actions>
     </md-card>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.gastro-overview {
+  padding: 20em;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.md-content {
+  width: 100px;
+  height: 100px;
+  margin: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 .md-card {
   width: 320px;
-  margin: 4px;
+  margin: 24px;
   display: inline-block;
   vertical-align: top;
 }
@@ -32,19 +48,35 @@ export default {
   name: "GastroOverview",
   data: function() {
     return {
-      gastro: {
-        name: "Reynabi",
-        contact: {
-          facebook: "https://www.facebook.com/KussmundGiessen/",
-          tel: "0641 2797720"
+      gastronomy: [
+        {
+          id: 1,
+          name: "Reynabi",
+          contact: {
+            facebook: "https://www.facebook.com/KussmundGiessen/",
+            tel: "0641 2797720"
+          },
+          address: {
+            postal_code: 35394,
+            street: "Grünberger Str.",
+            number: "115"
+          },
+          logo: "reynabi.png"
         },
-        address: {
-          postal_code: 35394,
-          street: "Grünberger Str.",
-          number: "115"
-        },
-        logo: "reynabi.png"
-      }
+        {
+          id: 2,
+          name: "Bärenstark",
+          contact: {
+            tel: "0641 97274191"
+          },
+          address: {
+            postal_code: 35390,
+            street: "Katharinengasse",
+            number: "14"
+          },
+          logo: "baerenstark.png"
+        }
+      ]
     };
   }
 };
