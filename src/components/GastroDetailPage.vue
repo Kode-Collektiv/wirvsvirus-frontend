1aacbf42946eb5e15ff42a621070082396ba6efe
niecore <nicomueller1991@googlemail.com>
2020-03-21 15:51:21 +0100

    Finish gastro detail page

--- src/components/GastroDetailPage.vue
+++ src/components/GastroDetailPage.vue
@@ -1,17 +1,34 @@
 <template>
-    <h1>{{gastro.name}}</h1>
+    <div class="gastro-detail">
+        <h1>{{gastro.name}}</h1>
+        <img
+                :src="require('../assets/img/gastro_logos/' + gastro.logo)"
+                height="300x"
+                width="300px"
+        >
+        <p>{{gastro.detail}}</p>
+
+        <div class="gastro-detail-contact">
+            <p v-if="gastro.contact.tel">Tel: {{gastro.contact.tel}}</p>
+            <p v-if="gastro.contact.email">Email: {{gastro.contact.email}}</p>
+            <p v-if="gastro.contact.instagram">Ig: {{gastro.contact.instagram}}</p>
+            <p v-if="gastro.contact.facebook">Facebook: {{gastro.contact.facebook}}</p>
+        </div>
+    </div>
+
 </template>
 
 <script>
+
     export default {
         name: "GastroDetailPage",
         data: function() {
             return {
                 gastro: {
                     name: "Reynabi",
+                    detail: "BAZAAR. Der etwas andere Supermarkt. Wer die Lust hat etwas außergewöhnliches zukochen ist bei uns richtig. Alles was Sie für die pakistanische, indische, persische, afghanische, afrikanische und südamerikanische Küche brauchen finden Sie bei uns.",
                     contact: {
-                        facebook: "https://www.facebook.com/KussmundGiessen/",
-                        tel: "0641 2797720"
+                        tel: "0641 2797720",
                     },
                     address: {
                         postal_code: 35394,
@@ -26,5 +43,7 @@
 </script>
 
 <style scoped>
-
+    .gastro-detail {
+        width: 300px;
+    }
 </style>
