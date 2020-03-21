e5f29f5ac1eafaa2cd73432ff4a40608feed1c9b
niecore <nicomueller1991@googlemail.com>
2020-03-21 18:32:04 +0100

    Voucher Amount Option now uses material design

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { MdCard, MdButton } from 'vue-material/dist/components'
+import { MdCard, MdButton, MdRadio, MdField } from 'vue-material/dist/components'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 
 Vue.config.productionTip = false
 
+Vue.use(MdField)
+Vue.use(MdRadio)
 Vue.use(MdCard)
 Vue.use(MdButton)
 
