9605d66cc51ba7202fa3420cc7f7dbc76260d61e
Felix Ebert <12857672+felix-ebert@users.noreply.github.com>
2020-03-22 13:26:25 +0100

    Improve banner in landing page

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="landing-page">
-    <div classe="greeting-banner">
-      <img :src="require('../assets/logo/helpnoweatlater.svg')" height="150px" width="150px" />
-      <h1>Wir sind ein Zusammenschluss gießener Gastronomen.</h1>
-      <p>Du bekommst hier schnell einen Überblick darüber, wie du an dein Lieblingsessen kommst. Mit klick auf die Logos erhälst du alle Informationen über die Liefermöglichkeiten der jeweiligen Läden.</p>
-      <md-button class="md-raised">Buy me a meal!</md-button>
+    <div class="greeting-banner">
+      <img class="logo" :src="require('../assets/logo/helpnoweatlater.svg')" />
+      <div class="title">
+        Jetzt bestellen.
+        <br />Gleich oder später genießen.
+      </div>
+      <div
+        class="subtitle"
+      >Mit dieser Seite bieten wir dir nicht nur die Möglichkeit dein gewohntes und geliebtes Essen zu bestellen. Du kannst auch mit dem Kauf eines Gutscheins dir oder bedürftigen Mitmenschen einen Gefallen tun oder mit einem Trinkgeld die lokale Gastronomie unterstützen.</div>
     </div>
 
     <GastroOverview />
@@ -16,6 +20,61 @@
   text-align: center;
   line-height: 2em;
   width: 100%;
+}
+
+.greeting-banner {
+  color: white;
+  background:  url(../assets/background/starfield.gif) no-repeat center center fixed; 
+  background-size: cover;
+}
+
+.logo {
+  height: 8em;
+  width: 8em;
+  color: white;
+}
+
+.title {
+  font-size: 4em;
+  letter-spacing: 0.1em;
+  line-height: 1em;
+  padding-top: 0.5em;
+}
+
+.subtitle {
+  font-size: 1.5em;
+  text-align: center;
+  padding: 1em 15em 1em 15em;
+}
+
+@media only screen and (max-width: 950px) {
+  .title {
+    font-size: 3em;
+    padding: 1em 2em 1em 2em;
+  }
+
+  .subtitle {
+    font-size: 1em;
+    padding: 1em 2em 1em 2em;
+  }
+}
+
+@media only screen and (max-width: 600px) {
+  .logo {
+    height: 6em;
+    width: 6em;
+  }
+
+  .title {
+    font-size: 2em;
+    padding: 0.5em 0 0.5em 0;
+  }
+
+  .subtitle {
+    font-size: 1em;
+    line-height: 1em;
+    padding: 0em 0em 1em 0em;
+  }
 }
 </style>
 
