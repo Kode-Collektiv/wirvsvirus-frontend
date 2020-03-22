51b6c91cd6557fc831497bd574517d75215e3e31
niecore <nicomueller1991@googlemail.com>
2020-03-22 01:07:20 +0100

    Add router

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,30 @@
     </md-app-toolbar>
 
     <md-app-content>
-      <GastroOverview />
+      <router-view/>
     </md-app-content>
   </md-app>
 </template>
 
-<script>
-import GastroOverview from "./components/GastroOverview.vue";
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: "App",
-  components: {
-    GastroOverview
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
   }
-};
-</script>
-
-<style>
-.md-app {
-  height: 100vh;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 import 'vue-material-design-icons-size/styles.css';
+import router from './router'
 
 Vue.config.productionTip = false
 
 Vue.use(VueMaterial)
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
