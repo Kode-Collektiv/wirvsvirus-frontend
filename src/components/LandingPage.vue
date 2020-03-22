11d63c114fe85dfd6ed9e75858b093fb3d50eaee
Felix Ebert <12857672+felix-ebert@users.noreply.github.com>
2020-03-22 01:15:42 +0100

    Add simple banner with logo

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="landing-page">
+    <div classe="greeting-banner">
+      <img :src="require('../assets/logo/helpnoweatlater.svg')" height="150px" width="150px" />
+      <h1>Wir sind ein Zusammenschluss gießener Gastronomen.</h1>
+      <p>Du bekommst hier schnell einen Überblick darüber, wie du an dein Lieblingsessen kommst. Mit klick auf die Logos erhälst du alle Informationen über die Liefermöglichkeiten der jeweiligen Läden.</p>
+      <md-button class="md-raised">Buy me a meal!</md-button>
+    </div>
+
+    <GastroOverview />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.landing-page {
+  text-align: center;
+  line-height: 2em;
+  width: 100%;
+}
+</style>
+
+<script>
+import GastroOverview from "./GastroOverview.vue";
+
+export default {
+  name: "LandingPage",
+  components: {
+    GastroOverview
+  }
+};
+</script>
