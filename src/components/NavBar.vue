151a73cad318ae4cecc40df52eb3491ecb31da54
Felix Ebert <12857672+felix-ebert@users.noreply.github.com>
2020-03-22 19:34:05 +0100

    Fix positioning of title in toolbar when back button is visible

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,7 +6,7 @@
           <img :src="require('../assets/icons/arrow_back-24px.svg')" />
         </md-button>
       </div>
-      <h3 class="md-title" style="flex: 1">GIESSEN TEILT AUS!</h3>
+      <h3 class="md-title" id="title">GIESSEN TEILT AUS!</h3>
     </div>
   </md-toolbar>
 </template>
@@ -19,6 +19,12 @@
   top: 0;
   width: 100%;
   z-index: 100;
+}
+
+#title {
+  text-align: center;
+  width: 100%;
+  position: absolute;
 }
 </style>
 
