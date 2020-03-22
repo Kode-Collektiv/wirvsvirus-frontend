f6239a1219b1e7c6c038d8dbcc74d1a93b3a5119
niecore <nicomueller1991@googlemail.com>
2020-03-22 02:10:10 +0100

    Link everything togheter

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <GastroOverview/>
+</template>
+
+<script>
+    import GastroOverview from "../components/GastroOverview.vue";
+
+    export default {
+        name: "Home",
+        components: {
+            GastroOverview
+        }
+    };
+</script>
+
+<style>
+  .md-app {
+    height: 100vh;
+  }
+</style>
