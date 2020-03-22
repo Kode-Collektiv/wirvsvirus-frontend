e007d5558cea39fa3c5e061307dbd50eadbf5f46
niecore <nicomueller1991@googlemail.com>
2020-03-22 21:44:16 +0100

    Add order button component

---
+++ src/components/OrderButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <md-button @click="onClickOrder" :disabled="this.disabled" class="md-raised md-primary">Jetzt Bestellen!</md-button>
+</template>
+
+<script>
+    const gastronomy = require("../model/database");
+
+    export default {
+        name: "OrderButton",
+        props: ['id'],
+        mounted() {
+            this.url = this.getGastroById(this.id).url;
+            this.disabled = this.url === undefined;
+        },
+        methods: {
+            getGastroById: function (id) {
+                return this.gastronomy.find(x => x.id == id);
+            },
+            onClickOrder: function () {
+                window.open(this.url);
+            },
+        },
+        data: function() {
+            return {
+                url: null,
+                disabled: true,
+                gastro: null,
+                gastronomy: gastronomy.gastronomy
+            };
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
